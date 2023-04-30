34fc1b0055242e366bb2b056251d97773178ca64
Vsollery <vsollery@gmail.com>
2023-04-30 12:14:01 +0700

    add components

---
+++ app/Http/Controllers/ListingController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    //Get All Listing
+    public function index(){
+
+    }
+    //Show SIngel Listing
+    public function show(){
+
+    }
+}
