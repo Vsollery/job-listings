4959e3af6dd509920f3c7d50f8c865992e6e7a33
Vsollery <vsollery@gmail.com>
2023-06-05 09:08:29 +0700

    add manage page

--- resources/views/listings/manage.blade.php
+++ resources/views/listings/manage.blade.php
@@ -8,47 +8,31 @@
 
         <table class="w-full table-auto rounded-sm">
             <tbody>
+                @foreach ($listings as $listing)
+                    
                 <tr class="border-gray-300">
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
                         <a href="show.html">
-                            Laravel Senior Developer
+                            {{ $listing->title }}
                         </a>
                     </td>
                     <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="edit.html" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                class="fa-solid fa-pen-to-square"></i>
+                        <a href="/listing/{{ $listing->id }}/edit" class="text-blue-400 px-6 py-2 rounded-xl"><i
+                            class="fa-solid fa-pen-to-square"></i>
                             Edit</a>
-                    </td>
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <form action="">
-                            <button class="text-red-600">
-                                <i class="fa-solid fa-trash-can"></i>
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
-
-                <tr class="border-gray-300">
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="show.html">
-                            Junior Developer Opening
-                        </a>
-                    </td>
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <a href="edit.html" class="text-blue-400 px-6 py-2 rounded-xl"><i
-                                class="fa-solid fa-pen-to-square"></i>
-                            Edit</a>
-                    </td>
-                    <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
-                        <form action="">
-                            <button class="text-red-600">
-                                <i class="fa-solid fa-trash-can"></i>
-                                Delete
-                            </button>
-                        </form>
-                    </td>
-                </tr>
+                        </td>
+                        <td class="px-4 py-8 border-t border-b border-gray-300 text-lg">
+                            <form action="/listing/{{ $listing->id }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button class="text-red-600">
+                                    <i class="fa-solid fa-trash-can"></i>
+                                    Delete
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach              
             </tbody>
         </table>
     </div>
--- resources/views/partials/_hero.blade.php
+++ resources/views/partials/_hero.blade.php
@@ -9,10 +9,18 @@
         <p class="text-2xl text-gray-200 font-bold my-4">
             Find or post Laravel jobs & projects
         </p>
-        <div>
-            <a href="/register"
-                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
-                Up to List a Gig</a>
-        </div>
+        @auth
+            <div>
+                <a href="/"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">
+                    Welcome </a>
+            </div>
+        @else
+            <div>
+                <a href="/register"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black">Sign
+                    Up to List a Gig</a>
+            </div>
+        @endauth
     </div>
 </section>
